b14e4acabd347bf28a69964950a9e58faab52d22
Anandraj <al.anandraj@gmail.com>
2018-03-17 17:48:54 +0530

    Dashboard circular progress bar gradient color added.

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 
 import CircularProgressbar from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+
+import GradientSVG from './GradientSVG';
+
 import '../css/Products.css';
 
 class Products extends Component {
@@ -13,16 +16,21 @@ class Products extends Component {
     render() {
         return (
             <div id="product">
-                <CircularProgressbar className = "bar1" percentage={90} initialAnimation counterClockwise strokeWidth = {10}
+                <CircularProgressbar className = "bar1" percentage={80} initialAnimation counterClockwise strokeWidth = {10}
                                      textForPercentage = {(pct) => "450"}/>
                 <CircularProgressbar className = "bar2" percentage={60} initialAnimation counterClockwise strokeWidth = {10}
                                      textForPercentage = {(pct) => "657"}/>
-                <CircularProgressbar className = "bar3" percentage={20} initialAnimation counterClockwise strokeWidth = {10}
+                <CircularProgressbar className = "bar3" percentage={80} initialAnimation counterClockwise strokeWidth = {10}
                                      textForPercentage = {(pct) => "1.2k"}/>
-                <CircularProgressbar className = "bar4" percentage={80} initialAnimation counterClockwise strokeWidth = {10}
+                <CircularProgressbar className = "bar4" percentage={60} initialAnimation counterClockwise strokeWidth = {10}
                                      textForPercentage = {(pct) => "1.8k"}/>
-                <CircularProgressbar className = "bar5" percentage={10} initialAnimation counterClockwise strokeWidth = {10}
+                <CircularProgressbar className = "bar5" percentage={70} initialAnimation counterClockwise strokeWidth = {10}
                                      textForPercentage = {(pct) => "2.4k"}/>
+                <GradientSVG startColor="#D4E6F1" endColor="#5DADE2" idCSS="bar1" rotation="90" />
+                <GradientSVG startColor="#7FB3D5" endColor="#3498DB" idCSS="bar2" rotation="90" />
+                <GradientSVG startColor="#F2F3F4" endColor="#C39BD3" idCSS="bar3" rotation="90" />
+                <GradientSVG startColor="#958CB6" endColor="#3817AF" idCSS="bar4" rotation="90" />
+                <GradientSVG startColor="#7FB3D5" endColor="#3498DB" idCSS="bar5" rotation="90" />
             </div>
         )
     }
