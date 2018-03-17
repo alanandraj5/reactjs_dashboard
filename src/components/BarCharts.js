8ee7ee1a8f11df6d7648e50046f8838d371a13ba
Anandraj <al.anandraj@gmail.com>
2018-03-17 20:00:13 +0530

    Dashboard: bar chart label css changes.

--- src/components/BarCharts.js
+++ src/components/BarCharts.js
@@ -1,26 +1,40 @@
 import React, {Component} from 'react';
 
 import {BarChart} from 'react-easy-chart';
+const $ = require('jquery');
 
-const BarCharts = () => {
-    return (
-        <div>
-            <BarChart
-                axes
-                colorBars
-                height={300}
-                width={300}
-                data={[
-                    { x: 'A', y: 20, color: '#00bcd4'},
-                    { x: 'B', y: 30, color: '#2196f3'},
-                    { x: 'C', y: 40, color: '#da91e6'},
-                    { x: 'D', y: 20, color: '#00bcd4'},
-                    { x: 'E', y: 40, color: '#2196f3'},
-                    { x: 'F', y: 25, color: '#da91e6'},
-                ]}
-            />
-        </div>
-    )
+class BarCharts extends Component {
+
+    componentDidMount = () => {
+        $('#bar-chart').find('.tick').find('text').attr('fill', '#fff');
+    };
+
+    render() {
+        return (
+            <div>
+                <div id="bar-chart">
+                    <BarChart
+                        axes
+                        colorBars
+                        height={300}
+                        width={300}
+                        xTickNumber={7}
+                        yTickNumber={7}
+                        data={[
+                            {x: 'MON', y: 35, color: '#00bcd4'},
+                            {x: 'TUE', y: 30, color: '#2196f3'},
+                            {x: 'WED', y: 40, color: '#da91e6'},
+                            {x: 'THU', y: 20, color: '#00bcd4'},
+                            {x: 'FRI', y: 50, color: '#2196f3'},
+                            {x: 'SAT', y: 25, color: '#da91e6'},
+                            {x: 'SUN', y: 15, color: '#00bcd4'},
+                        ]}
+                    />
+                </div>
+
+            </div>
+        )
+    }
 };
 
 export default BarCharts;
--- src/components/GradientSVG.js
+++ src/components/GradientSVG.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 const GradientSVG = (props) => {
     let { startColor, endColor, idCSS, rotation } = props;
